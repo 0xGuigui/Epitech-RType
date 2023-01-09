efcba1282187c41db97eaaaac6172a908a90bc62
Gagonlaire <paul.gervais@epitech.eu>
2023-01-09 11:56:51 +0100

    Add basic project architecture

---
+++ lib/src/example.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Paul Gervais on 09/01/2023.
+//
+
+#include <iostream>
+
+void helloWorld() {
+    std::cout << "Hello World" << std::endl;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Paul Gervais on 09/01/2023.
+//
+
+#include <iostream>
+#include "lib/lib.h"
+
+int main() {
+    helloWorld();
+    std::cout << "Hello from server" << std::endl;
+}
